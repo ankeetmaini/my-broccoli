ec048c7675f47dd381d26c46cebd9d9ed605cdf7
Ankeet Maini <ankeet.maini@gmail.com>
2015-03-15 19:39:36 +0530

    added css files and exported it after concatenating with Broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,18 @@
 // a plugin which concatenates the files
 var concat = require('broccoli-concat');
+var mergeTrees = require('broccoli-merge-trees');
 
+// concatenating all JS files
 var javascripts = concat('js', {
   inputFiles: ['**/*.js'],
   outputFile: '/assets/app.js'
 });
 
-module.exports = javascripts;
+// concatenating all CSS files
+var css = concat('styles', {
+  inputFiles: ['**/*.css'],
+  outputFile: '/assets/app.css'
+});
+
+// exporting both JS tree and css as well
+module.exports = mergeTrees([javascripts, css]);
